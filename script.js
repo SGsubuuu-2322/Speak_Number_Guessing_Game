6f094ab5d445cc8986e0df0cbb3088df57de68c4
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-02 01:02:38 +0530

    Added the script for recognizing my voice by event listener...

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ let randomNumber = getRandomNumber();
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
+
 let recognition = new window.SpeechRecognition();
 
 recognition.start();
 
+function onSpeak(e) {
+  console.log(e);
+}
+
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
+
+// console.log(recognition);
+recognition.addEventListener("result", onSpeak);
